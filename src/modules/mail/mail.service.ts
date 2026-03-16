41eeff62e346366775c3e90f7924a23a2961b0bf
bsaygili <bhdrsaygili@gmail.com>
2026-03-16 23:54:02 +0300

    fix: render.com mail issue been fixed

--- src/modules/mail/mail.service.ts
+++ src/modules/mail/mail.service.ts
@@ -1,17 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import * as fs from 'fs';
 import * as mustache from 'mustache';
-import nodemailer, { Transporter } from 'nodemailer';
 import path from 'path';
+import { Resend } from 'resend';
 import { CriticalProduct } from '../critical/critical.service';
 
 @Injectable()
 export class MailService {
-    private transporter;
-
+    private resend = new Resend(process.env.RESEND_API_KEY);
     constructor() {}
 
     async sendCriticalStockMail(emails: string[], products: CriticalProduct[]) {
+        console.log('emails', emails);
         if (!products.length) return;
 
         // __dirname çalışma zamanı ortamına göre dist/… ya da src/… olur.
@@ -41,6 +41,13 @@ export class MailService {
             criticalProducts: products,
         });
 
+        if (!process.env.RESEND_API_KEY) {
+            throw new Error('RESEND_API_KEY missing');
+        }
+
+        if (!process.env.RESEND_API_KEY) {
+            throw new Error('RESEND_API_KEY missing');
+        }
         const emailUser = process.env.EMAIL;
         const emailPass = process.env.PASSWORD;
         const notificationRecipients = emails?.join(',');
@@ -51,21 +58,30 @@ export class MailService {
             );
         }
 
-        const transporter: Transporter = nodemailer.createTransport({
-            host: 'smtp.gmail.com',
-            port: 587,
-            secure: false,
-            family: 4, // IPv4 zorlar
-            auth: {
-                user: emailUser,
-                pass: emailPass,
-            },
-        });
+        // const transporter: Transporter = nodemailer.createTransport({
+        //     host: 'smtp.gmail.com',
+        //     port: 587,
+        //     secure: false,
+        //     family: 4, // IPv4 zorlar
+        //     auth: {
+        //         user: emailUser,
+        //         pass: emailPass,
+        //     },
+        // });
 
-        await transporter.sendMail({
-            to: notificationRecipients,
-            subject: 'Kritik Stok Uyarısı',
+        await this.resend.emails.send({
+            // from: emailUser,
+            from: 'Saygili Stock <onboarding@resend.dev>',
+            to: ['bhdrsaygili@gmail.com'], //emails,
+            subject: '⚠️ Kritik Stok Uyarısı',
             html,
         });
+        console.log('✅ Critical stock email sent');
+
+        // await transporter.sendMail({
+        //     to: notificationRecipients,
+        //     subject: 'Kritik Stok Uyarısı',
+        //     html,
+        // });
     }
 }
